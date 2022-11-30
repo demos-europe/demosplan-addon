88ac7ef7992429fab5e260d2e7a7d65a98947181
Christian Dressler <dressler@demos-deutschland.de>
2022-11-30 13:52:08 +0100

    feature (refs T29879): add permission filter validators

---
+++ src/Permission/Validation/PermissionFilterValidatorInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DemosEurope\DemosplanAddon\Permission\Validation;
+
+interface PermissionFilterValidatorInterface
+{
+    /**
+     * @param mixed $filter the filter needs to correspond to a specific format, but just for the
+     *                      validation any kind of value is allowed here
+     *
+     * @throws PermissionFilterException
+     */
+    public function validateFilter($value): void;
+}
