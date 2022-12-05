6020c3e6acb03b2eaa4c7b3779ccdf81d0a21f67
Hamza Bakri <bakri@demos-deutschland.de>
2022-12-05 14:01:08 +0100

    Fix (ref: T30254): add annotations

--- src/Contracts/StatementCreatorInterface.php
+++ src/Contracts/StatementCreatorInterface.php
@@ -7,6 +7,11 @@ namespace DemosEurope\DemosplanAddon\Contracts;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * This interface will be implemented by all statementsCreators in addons
+ *
+ * This interface will be implemented only in addons and not in the core.
+ */
 interface StatementCreatorInterface
 {
     public function __invoke( Request $request, string $procedureId): Response;
