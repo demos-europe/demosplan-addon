ac837d61c5872b0ccd7456a802595583db717e1e
Hamza Bakri <bakri@demos-deutschland.de>
2022-12-12 20:53:27 +0100

    Fix (ref: T30254): add JsonSchemaValidator
    - this class was moved from core to demosplan addon
    
    (ADO 1740)

---
+++ src/Validator/JsonSchemaValidator.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace DemosEurope\DemosplanAddon\Validator;
+
+use DemosEurope\DemosplanAddon\DemosPipes\Logic\Json;
+use JsonException;
+use JsonSchema\Exception\InvalidSchemaException;
+use JsonSchema\Validator;
+use Symfony\Component\Filesystem\Exception\FileNotFoundException;
+
+class JsonSchemaValidator
+{
+    /**
+     * @throws InvalidSchemaException On invalid content
+     * @throws JsonException          On invalid schema or json object file
+     */
+    public function validate(string $json, string $schemaPath): void
+    {
+        if (!$jsonSchema = file_get_contents($schemaPath)) {
+            throw new FileNotFoundException('File not found in path: ' . $schemaPath);
+        }
+
+        $validator = new Validator();
+
+        $schemaObject = Json::decodeToMatchingType($jsonSchema);
+        $jsonObject = Json::decodeToMatchingType($json);
+
+        $validator->validate($jsonObject, $schemaObject);
+
+        if (!$validator->isValid()) {
+            $errorMsgs = [];
+
+            foreach ($validator->getErrors() as $error) {
+                $errorMsg = 'Json Schema Error. Field "' . $error['property'] . '". ';
+                $errorMsg .= $error['message'];
+                $errorMsgs[] = $errorMsg;
+            }
+
+            $errorMsg = implode("\n", $errorMsgs);
+            throw new InvalidSchemaException($errorMsg);
+        }
+    }
+}
+
