55e6f7b23c44bd663102efd154c758c5b987f025
Christian Dressler <107701274+dresslerdemos@users.noreply.github.com>
2022-12-16 11:33:44 +0100

    feature (refs T29879): flesh out permission syntax (#6)
    * fix (refs T29879): denote and handle types correctly
    
    * refactor (refs T29879): improve naming and documentation
    
    * feature (refs T29879): add permission condition helpers
    
    * fix (refs T29879): require runtime dependencies
    
    * chore (refs T29879): improve documentation

--- src/Permission/PermissionCondition.php
+++ src/Permission/PermissionCondition.php
@@ -4,23 +4,27 @@ declare(strict_types=1);
 
 namespace DemosEurope\DemosplanAddon\Permission;
 
+use DemosEurope\DemosplanAddon\Permission\Conditions\AbstractCondition;
 use Symfony\Component\Validator\Constraints as Assert;
 use DemosEurope\DemosplanAddon\Permission\Validation\PermissionFilterConstraint;
 
 /**
  * Provides conditions to determine if a permission should be enabled or not.
  *
+ * Use instances of this class when calling {@link ResolvablePermissionCollectionInterface::configurePermission()}
+ * within your {@link AbstractPermissionEvaluator::configureAddonPermissions()} implementation.
+ *
  * For a permission to be considered enabled by an instance of this class **all** of the following
  * must be true:
+ *
  * * The conditions returned by the {@link self::getCustomerConditions()} must match the current
- * customer context. If the current customer is `null` there must be effectively no customer
- * conditions present.
+ * customer context or be effectively empty.
+ *
  * * The conditions returned by the {@link self::getUserConditions()} must match the current
- * user context. If the current user is `null` there must be effectively no user conditions
- * present.
+ * user context or be effectively empty.
+ *
  * * The conditions returned by the {@link self::getProcedureConditions()} must match the current
- * procedure context. If the current procedure is `null` there must be effectively no procedure
- * conditions present.
+ * procedure context or be effectively empty.
  *
  * Having *effectively* no conditions present means that the returned array does not need to be
  * empty. It may contain empty groups or conditions that are members of non-existent groups only.
@@ -32,46 +36,69 @@ use DemosEurope\DemosplanAddon\Permission\Validation\PermissionFilterConstraint;
 class PermissionCondition
 {
     /**
-     * @var PermissionFilter
+     * @var array<non-empty-string, PermissionFilter>
      *
-     * @Assert\NotNull()
-     * @Assert\Type(type="array")
-     * @PermissionFilterConstraint()
+     * @Assert\All([
+     *     @Assert\NotNull(),
+     *     @Assert\Type(type="array"),
+     *     @PermissionFilterConstraint()
+     * ])
      */
     private array $customerConditions;
 
     /**
      * @var array<non-empty-string, PermissionFilter>
      *
-     * @Assert\NotNull()
-     * @Assert\Type(type="array")
-     * @PermissionFilterConstraint()
+     * @Assert\All([
+     *     @Assert\NotNull(),
+     *     @Assert\Type(type="array"),
+     *     @PermissionFilterConstraint()
+     * ])
      */
     private array $userConditions;
 
     /**
      * @var array<non-empty-string, PermissionFilter>
      *
-     * @Assert\NotNull()
-     * @Assert\Type(type="array")
-     * @PermissionFilterConstraint()
+     * @Assert\All([
+     *     @Assert\NotNull(),
+     *     @Assert\Type(type="array"),
+     *     @PermissionFilterConstraint()
+     * ])
      */
     private array $procedureConditions;
 
     /**
-     * @param PermissionFilter $customerConditions
-     * @param PermissionFilter $userConditions
-     * @param PermissionFilter $procedureConditions
+     * Initialize an instance with the given conditions.
+     *
+     * The `PermissionFilter` format extends the Drupal filter
+     * {@link https://www.drupal.org/docs/core-modules-and-themes/core-modules/jsonapi-module/filtering specification}.
+     *
+     * Beside additional operators it also adds support for the `parameterCondition` type, which
+     * works like the `condition` type, except that it does not contain a `value` field with a predefined
+     * value, but a `parameter`field. The `parameter` field accepts specific placeholders, which are
+     * replaced with the actual value when the permission is evaluated. For supported placeholders
+     * and other details see the `PermissionFilter` format type-definition.
+     *
+     * @param array<non-empty-string, PermissionFilter>|AbstractCondition $customerConditions
+     * @param array<non-empty-string, PermissionFilter>|AbstractCondition $userConditions
+     * @param array<non-empty-string, PermissionFilter>|AbstractCondition $procedureConditions
      */
-    public function __construct(array $customerConditions, array $userConditions, array $procedureConditions)
+    public function __construct($customerConditions, $userConditions, $procedureConditions)
     {
-        $this->customerConditions = $customerConditions;
-        $this->userConditions = $userConditions;
-        $this->procedureConditions = $procedureConditions;
+        $this->customerConditions = $customerConditions instanceof AbstractCondition
+            ? $customerConditions->toCondition()
+            : $customerConditions;
+        $this->userConditions = $userConditions instanceof AbstractCondition
+            ? $userConditions->toCondition()
+            : $userConditions;
+        $this->procedureConditions =$procedureConditions instanceof AbstractCondition
+            ? $procedureConditions->toCondition()
+            : $procedureConditions;
     }
 
     /**
-     * @return PermissionFilter
+     * @return array<non-empty-string, PermissionFilter>
      */
     public function getProcedureConditions(): array
     {
@@ -79,7 +106,7 @@ class PermissionCondition
     }
 
     /**
-     * @return PermissionFilter
+     * @return array<non-empty-string, PermissionFilter>
      */
     public function getUserConditions(): array
     {
@@ -87,7 +114,7 @@ class PermissionCondition
     }
 
     /**
-     * @return PermissionFilter
+     * @return array<non-empty-string, PermissionFilter>
      */
     public function getCustomerConditions(): array
     {
