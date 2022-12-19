2b3e6c46154e94df636cade5694d7b4ef49a202c
bakridemos <102021913+bakridemos@users.noreply.github.com>
2022-12-19 10:30:52 +0100

    Fix (ref: T30254): add contracts (#2)
    * Fix (ref: T30254): add contracts
    
    - CreateSimplifiedStatementEventInterface.php
    - StatementCreatorInterface.php
    
    (ADO 1740)
    
    * Fix (ref: T30254): add annotations

---
+++ src/Contracts/CreateSimplifiedStatementEventInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DemosEurope\DemosplanAddon\Contracts;
+
+/**
+ * This event will be triggered in core when creating a new statement from a simplified form.
+ * This event interface allow to retrieve an instance from the StatementFromEmailCreator to create a statement
+ * from email when the statement is imported via email.
+ *
+ * This interface will be implemented only in core.
+ */
+interface CreateSimplifiedStatementEventInterface
+{
+
+}
