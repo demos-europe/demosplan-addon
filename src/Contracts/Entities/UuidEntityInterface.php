bd0601d4873cbd3c0483f7515ba2426e1e96b030
Hamza Bakri <bakri@demos-deutschland.de>
2022-12-20 09:25:44 +0100

    Fix (ref: T30254): adjust UuidEntityInterface
    - add inheritance
    
    (ADO 1740)

--- src/Contracts/Entities/UuidEntityInterface.php
+++ src/Contracts/Entities/UuidEntityInterface.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace DemosEurope\DemosplanAddon\Contracts\Entities;
 
-interface UuidEntityInterface
+interface UuidEntityInterface extends EntityInterface
 {
-
+    public function getId(): ?string;
 }
