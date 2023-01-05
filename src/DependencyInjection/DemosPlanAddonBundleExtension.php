c49da15585047e34dc20c1f6f2a6552385ee842c
AlexMenschoid <73840434+AlexMenschoid@users.noreply.github.com>
2023-01-05 13:12:08 +0100

    feat: bundles addon up (#19)
    * feat: bundles addon up
    
    This should make the whole addon dependency loadable as a Symfony bundle which makes it easier for it to bring along its own service definitions.
    
    * fix: renames to DemosPlanAddonBundle
    
    * fix: removes obsolete service definitions

---
+++ src/DependencyInjection/DemosPlanAddonBundleExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DemosEurope\DemosplanAddon\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class DemosPlanAddonBundleExtension extends Extension
+{
+
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../../config')
+        );
+        $loader->load('services.yml');
+    }
+}
