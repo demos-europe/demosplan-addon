d5229b5851806c6a8e3c392c57ea441b0417a870
Hamza Bakri <bakri@demos-deutschland.de>
2023-01-23 08:45:34 +0100

    Fix (ref: T30254): add contracts
    - add UserResourceTypeInterface
    
    (ADO 1740)

---
+++ src/Contracts/ResourceType/UserResourceTypeInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DemosEurope\DemosplanAddon\Contracts\ResourceType;
+
+use EDT\JsonApi\ResourceTypes\ResourceTypeInterface;
+use EDT\PathBuilding\PropertyAutoPathInterface;
+
+interface UserResourceTypeInterface extends PropertyAutoPathInterface, ResourceTypeInterface
+{
+
+}
