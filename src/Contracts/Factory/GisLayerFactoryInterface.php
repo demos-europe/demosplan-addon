875c2821b77da25c6de0355372afdb19b6b6e6b3
Hamza Bakri <bakri@demos-deutschland.de>
2023-02-28 13:48:34 +0100

    F(ref: T31336): add missing use statement

--- src/Contracts/Factory/GisLayerFactoryInterface.php
+++ src/Contracts/Factory/GisLayerFactoryInterface.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace DemosEurope\DemosplanAddon\Contracts\Factory;
 
+use DemosEurope\DemosplanAddon\Contracts\Entities\GisLayerInterface;
+
 interface GisLayerFactoryInterface
 {
     public function createGisLayer(): GisLayerInterface;
