455cfe82ace3f298a72af88a7ad5f7225ff3fea9
bakridemos <102021913+bakridemos@users.noreply.github.com>
2023-03-28 14:43:45 +0200

    F add event contract (#38)
    * F: add HandleEmailAddressesEventInterface
    
    * F: add missing Templates parameters

--- src/Contracts/ResourceType/OriginalStatementResourceTypeInterface.php
+++ src/Contracts/ResourceType/OriginalStatementResourceTypeInterface.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 
 namespace DemosEurope\DemosplanAddon\Contracts\ResourceType;
 
+use DemosEurope\DemosplanAddon\Contracts\Entities\StatementInterface;
+use EDT\DqlQuerying\Contracts\ClauseFunctionInterface;
+use EDT\DqlQuerying\Contracts\OrderBySortMethodInterface;
 use EDT\JsonApi\ResourceTypes\ResourceTypeInterface;
 use EDT\PathBuilding\PropertyAutoPathInterface;
 
+/**
+ * @template-extends ResourceTypeInterface<ClauseFunctionInterface<bool>, OrderBySortMethodInterface, StatementInterface>
+ */
 interface OriginalStatementResourceTypeInterface extends PropertyAutoPathInterface, ResourceTypeInterface
 {
 
