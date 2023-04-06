d4942bbf77b3523793682554920e3aed77668448
MoritzMandler <89914798+MoritzMandler@users.noreply.github.com>
2023-04-06 09:34:29 +0200

    feature (refs T32054) Remove an unused (#45)
    EventInterface and add implemented methods to
    EmailAddressRepositoryInterface

--- src/Contracts/Repositories/EmailAddressRepositoryInterface.php
+++ src/Contracts/Repositories/EmailAddressRepositoryInterface.php
@@ -4,7 +4,24 @@ declare(strict_types=1);
 
 namespace DemosEurope\DemosplanAddon\Contracts\Repositories;
 
+use DemosEurope\DemosplanAddon\Contracts\Entities\EmailAddressInterface;
+
 interface EmailAddressRepositoryInterface
 {
+    public function getOrCreateEmailAddress(string $inputEmailAddressString): EmailAddressInterface;
 
+    /**
+     * @param string[] $inputEmailAddressStrings
+     *
+     * @return EmailAddressInterface[] emailAddress entities with the given $inputEmailAddressString as
+     *                        keys in the array and stored as their fullAddresses
+     */
+    public function getOrCreateEmailAddresses(array $inputEmailAddressStrings): array;
+
+    /**
+     * Checks if any EmailAddress entities are not referenced anymore and if so deletes them.
+     *
+     * @return int the number of deletions
+     */
+    public function deleteOrphanEmailAddresses(): int;
 }
