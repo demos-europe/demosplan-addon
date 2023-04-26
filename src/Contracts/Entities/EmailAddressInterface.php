bb91bf52272c362df3f9114417868c8ab2246f5b
Moritz Mandler <mandler@demos-deutschland.de>
2023-04-26 17:41:51 +0200

    feature (refs T32212) Methods where added to EmailAddressInterface.

--- src/Contracts/Entities/EmailAddressInterface.php
+++ src/Contracts/Entities/EmailAddressInterface.php
@@ -4,7 +4,19 @@ declare(strict_types=1);
 
 namespace DemosEurope\DemosplanAddon\Contracts\Entities;
 
-interface EmailAddressInterface
+interface EmailAddressInterface extends CoreEntityInterface, UuidEntityInterface
 {
+    public function setId(string $id): void;
 
+    /**
+     * The full email address containing the local part and domain name.
+     * Not the so-called display name.
+     **/
+    public function getFullAddress(): string;
+
+    /**
+    * The full email address containing the local part and domain name.
+    * Not the so-called display name.
+    **/
+    public function setFullAddress(string $fullAddress);
 }
