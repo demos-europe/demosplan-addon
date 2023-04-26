0cea9d9bfcf515a5c2d43a8110da60c1bfc5277f
Moritz Mandler <mandler@demos-deutschland.de>
2023-04-26 11:02:36 +0200

    feature (refs T32212) the ApiResourceServiceInterface needs to know about the BaseTransformer for its methods signatures. Not possible - so a BaseTransformerInterface was created and implemented by the Basetransformer and used instead. Methods where added to ApiResourceServiceInterface.

--- src/Contracts/ApiRequest/ApiResourceServiceInterface.php
+++ src/Contracts/ApiRequest/ApiResourceServiceInterface.php
@@ -4,7 +4,57 @@ declare(strict_types=1);
 
 namespace DemosEurope\DemosplanAddon\Contracts\ApiRequest;
 
+use DemosEurope\DemosplanAddon\Contracts\Entities\CoreEntityInterface;
+use DemosEurope\DemosplanAddon\Contracts\ValueObject\ValueObjectInterface;
+use DemosEurope\DemosplanAddon\Logic\ApiRequest\Transformer\BaseTransformerInterface;
+use Exception;
+use League\Fractal\Manager;
+use League\Fractal\Resource\Collection;
+use League\Fractal\Resource\Item;
+use Symfony\Component\Security\Core\User\UserInterface;
+
 interface ApiResourceServiceInterface
 {
+    public function getFractal(): Manager;
 
+    /**
+     * @param string $transformerName #Service Service name or fq class name of the transformer
+     *
+     * @throws Exception
+     */
+    public function getTransformer(string $transformerName): BaseTransformerInterface;
+
+    /**
+     * @param iterable|CoreEntityInterface[]|ValueObjectInterface[] $data
+     * @param string $transformerName #Service Service name or fq class name of the transformer
+     * @param string $type optionally override the item type
+     */
+    public function makeCollection($data, string $transformerName, $type = ''): Collection;
+
+    /**
+     * @param                           $data
+     * @param BaseTransformerInterface  $baseTransformer
+     * @param                           $type
+     *
+     * @return Collection
+     */
+    public function makeAddonCollection($data, BaseTransformerInterface $baseTransformer, $type = ''): Collection;
+
+    /**
+     * @param iterable|CoreEntityInterface[]|ValueObjectInterface[] $data
+     * @param string $resourceTypeName The value returned by {@link ResourceTypeInterface::getName()}
+     */
+    public function makeCollectionOfResources($data, string $resourceTypeName): Collection;
+
+    /**
+     * @param array|CoreEntityInterface|ValueObjectInterface|UserInterface $data
+     * @param string $transformerName #Service name or fq class name of the transformer
+     * @param string $type optionally override the item type
+     */
+    public function makeItem($data, string $transformerName, $type = ''): Item;
+
+    /**
+     * @param string $resourceTypeName The value returned by {@link ResourceTypeInterface::getName()}
+     */
+    public function makeItemOfResource($data, string $resourceTypeName): Item;
 }
---
+++ src/Logic/ApiRequest/Transformer/BaseTransformerInterface.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DemosEurope\DemosplanAddon\Logic\ApiRequest\Transformer;
+
+use DateTime;
+use DemosEurope\DemosplanAddon\Contracts\ApiRequest\ApiResourceServiceInterface;
+use DemosEurope\DemosplanAddon\Contracts\Config\GlobalConfigInterface;
+use DemosEurope\DemosplanAddon\Contracts\PermissionsInterface;
+
+interface BaseTransformerInterface
+{
+    public function getClass(): string;
+
+    /**
+     * @deprecated use {@link ResourceTypeInterface::getName()} where possible, because it should
+     *             be the actual source for this information and not the transformer
+     */
+    public function getType(): string;
+
+    public function getPermissions(): PermissionsInterface;
+
+    /**
+     * Please don't use `@required` for DI. It should only be used in base classes like this one.
+     *
+     * @required
+     */
+    public function setPermissions(PermissionsInterface $permissions): void;
+
+    /**
+     * @param DateTime $date
+     */
+    public function formatDate($date): string;
+
+    /**
+     * Please don't use `@required` for DI. It should only be used in base classes like this one.
+     *
+     * @required
+     */
+    public function setResourceService(ApiResourceServiceInterface $resourceService): void;
+
+    public function getGlobalConfig(): GlobalConfigInterface;
+
+    /**
+     * Please don't use `@required` for DI. It should only be used in base classes like this one.
+     *
+     * @required
+     */
+    public function setGlobalConfig(GlobalConfigInterface $globalConfig): void;
+
+}
