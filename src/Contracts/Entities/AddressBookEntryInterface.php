164bc038ee0845baf6e99877058fcb542fefe4a1
Moritz Mandler <mandler@demos-deutschland.de>
2023-04-28 16:29:56 +0200

    feature (refs T32212) adds methods to the AddressBookEntryInterface

--- src/Contracts/Entities/AddressBookEntryInterface.php
+++ src/Contracts/Entities/AddressBookEntryInterface.php
@@ -2,7 +2,39 @@
 
 namespace DemosEurope\DemosplanAddon\Contracts\Entities;
 
+use DateTime;
+use DateTimeImmutable;
+use DateTimeInterface;
+
 interface AddressBookEntryInterface extends UuidEntityInterface
 {
+    public function getName(): ?string;
 
+    /**
+     * @param string|null $name
+     */
+    public function setName($name);
+
+    public function getEmailAddress(): string;
+
+    public function setEmailAddress(string $emailAddress);
+
+    /**
+     * @return DateTime|DateTimeImmutable
+     */
+    public function getCreatedDate(): DateTimeInterface;
+
+    /**
+     * @param DateTime|DateTimeImmutable $createdDate
+     */
+    public function setCreatedDate(DateTimeInterface $createdDate);
+
+    /**
+     * @return DateTime|DateTimeImmutable
+     */
+    public function getModifiedDate(): DateTimeInterface;
+
+    public function getOrganisation(): OrgaInterface;
+
+    public function setOrganisation(OrgaInterface $organisation);
 }
