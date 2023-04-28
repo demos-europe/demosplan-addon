ade9c37193f743d58f60889bdf622329153e3ef9
Moritz Mandler <mandler@demos-deutschland.de>
2023-04-28 18:20:32 +0200

    feature (refs T32212) adds methods to the SlugInterface and SluggedEntityInterface.

--- src/Contracts/Entities/SlugInterface.php
+++ src/Contracts/Entities/SlugInterface.php
@@ -4,5 +4,9 @@ namespace DemosEurope\DemosplanAddon\Contracts\Entities;
 
 interface SlugInterface extends UuidEntityInterface
 {
+    public function setId(string $id);
 
+    public function getName(): string;
+
+    public function setName(string $name);
 }
--- src/Contracts/Entities/SluggedEntityInterface.php
+++ src/Contracts/Entities/SluggedEntityInterface.php
@@ -2,7 +2,35 @@
 
 namespace DemosEurope\DemosplanAddon\Contracts\Entities;
 
+use Doctrine\Common\Collections\Collection;
+use Exception;
+
 interface SluggedEntityInterface extends UuidEntityInterface
 {
+    public function getSlugs(): Collection;
 
+    public function setSlugs(Collection $slugs);
+
+    /**
+     * Given a Slug object adds it to the Entity, updating its slugs history and sets it as current Slug.
+     *
+     * @return SluggedEntityInterface
+     */
+    public function addSlug(SlugInterface $slug);
+
+    public function getCurrentSlug(): SlugInterface;
+
+    /**
+     * @throws Exception
+     */
+    public function setCurrentSlug(SlugInterface $currentSlug);
+
+    /**
+     * Returns true if the Orga already had the received slug, false otherwise.
+     */
+    public function hasSlugString(SlugInterface $slug): bool;
+
+    public function isSlugCurrent(string $slug): bool;
+
+    public function setInitialSlug();
 }
