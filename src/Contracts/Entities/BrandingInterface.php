5c8aab9f87ae9d659ceca4e6f686701998e4929f
Moritz Mandler <mandler@demos-deutschland.de>
2023-04-28 16:48:26 +0200

    feature (refs T32212) adds methods to the BrandingInterface.

--- src/Contracts/Entities/BrandingInterface.php
+++ src/Contracts/Entities/BrandingInterface.php
@@ -4,5 +4,11 @@ namespace DemosEurope\DemosplanAddon\Contracts\Entities;
 
 interface BrandingInterface extends UuidEntityInterface
 {
+    public function getCssvars(): ?string;
 
+    public function setCssvars(?string $cssVars): self;
+
+    public function getLogo(): ?FileInterface;
+
+    public function setLogo(?FileInterface $logo): void;
 }
