11d554bb91d8c8f2c8c68421e8e698cdaedce84c
Saeed Jalalifar <jalalifar@demos-deutschland.de>
2023-05-19 09:03:24 +0200

    feature (refs T32212): update OrgaStatusInCustomer Interface

--- src/Contracts/Entities/OrgaStatusInCustomerInterface.php
+++ src/Contracts/Entities/OrgaStatusInCustomerInterface.php
@@ -4,5 +4,27 @@ namespace DemosEurope\DemosplanAddon\Contracts\Entities;
 
 interface OrgaStatusInCustomerInterface extends UuidEntityInterface
 {
+    public const STATUS_PENDING = 'pending';
 
+    public const STATUS_ACCEPTED = 'accepted';
+    
+    public const STATUS_REJECTED = 'rejected';
+
+    public function setId(string $id);
+
+    public function getOrga(): OrgaInterface;
+
+    public function setOrga(OrgaInterface $orga);
+
+    public function getOrgaType(): OrgaTypeInterface;
+
+    public function setOrgaType(OrgaTypeInterface $orgaType);
+
+    public function getCustomer(): CustomerInterface;
+
+    public function setCustomer(CustomerInterface $customer);
+
+    public function getStatus(): string;
+
+    public function setStatus(string $status);
 }
