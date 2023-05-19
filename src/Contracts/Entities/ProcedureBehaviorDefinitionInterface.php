73c5c6b6fcaeaffabad3dd17edebff69836a1edd
Saeed Jalalifar <jalalifar@demos-deutschland.de>
2023-05-19 09:38:55 +0200

    feature (refs T32212): update ProcedureBehaviorDefinition Interface

---
+++ src/Contracts/Entities/ProcedureBehaviorDefinitionInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace DemosEurope\DemosplanAddon\Contracts\Entities;
+
+use DateTime;
+
+interface ProcedureBehaviorDefinitionInterface extends UuidEntityInterface
+{
+    public function getProcedure(): ?ProcedureInterface;
+
+    public function setProcedure(ProcedureInterface $procedure): void;
+
+    public function getProcedureType(): ?ProcedureTypeInterface;
+
+    public function setProcedureType(ProcedureTypeInterface $procedureType): void;
+
+    public function isAllowedToEnableMap(): bool;
+
+    public function setAllowedToEnableMap(bool $allowedToEnableMap): void;
+
+    public function hasPriorityArea(): bool;
+
+    public function setHasPriorityArea(bool $hasPriorityArea): void;
+
+    public function isParticipationGuestOnly(): bool;
+
+    public function setParticipationGuestOnly(bool $participationGuestOnly): void;
+
+    public function getCreationDate(): DateTime;
+
+    public function getModificationDate(): DateTime;
+
+}
