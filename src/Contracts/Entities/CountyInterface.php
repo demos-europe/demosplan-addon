ba01853970d9cedc24c8d9a46d46bff4bc3fa85f
Saeed Jalalifar <jalalifar@demos-deutschland.de>
2023-05-19 08:40:13 +0200

    feature (refs T32212): add new necessary County Interface

---
+++ src/Contracts/Entities/CountyInterface.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace DemosEurope\DemosplanAddon\Contracts\Entities;
+
+use League\Fractal\Resource\ArrayCollection;
+use League\Fractal\Resource\Collection;
+
+interface CountyInterface extends UuidEntityInterface
+{
+    /**
+     * @return Collection<int, CustomerCountyInterface>
+     */
+    public function getCustomerCounties(): Collection;
+
+    /**
+     * @param Collection<int, CustomerCountyInterface> $customerCounties
+     */
+    public function setCustomerCounties(Collection $customerCounties): void;
+
+    /**
+     * @return string
+     */
+    public function getName();
+
+    /**
+     * @param string $name
+     */
+    public function setName($name);
+
+    /**
+     * @return string
+     */
+    public function getEmail();
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email);
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getStatements();
+
+    /**
+     * @param ArrayCollection $statements
+     */
+    public function setStatements($statements);
+
+    /**
+     * Add Statement.
+     *
+     * @param StatementInterface $statement
+     *
+     * @return bool - true if the given statement was added to this county, otherwise false
+     */
+    public function addStatement($statement);
+
+    /**
+     * Remove Statement.
+     *
+     * @param StatementInterface $statement
+     */
+    public function removeStatement($statement);
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getStatementFragments();
+
+    /**
+     * Add StatementFragment.
+     *
+     * @param StatementFragmentInterface $fragment
+     */
+    public function addStatementFragment($fragment);
+
+    /**
+     * Remove StatementFragment.
+     *
+     * @param StatementFragmentInterface $fragment
+     */
+    public function removeStatementFragment($fragment);
+
+}
