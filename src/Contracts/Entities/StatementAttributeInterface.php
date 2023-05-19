d85ac69d7ee8e9a3e4a819ac973243b87873ad1a
Saeed Jalalifar <jalalifar@demos-deutschland.de>
2023-05-19 10:26:56 +0200

    feature (refs T32212): add necessary StatementAttribute Interface

---
+++ src/Contracts/Entities/StatementAttributeInterface.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace DemosEurope\DemosplanAddon\Contracts\Entities;
+
+interface StatementAttributeInterface extends UuidEntityInterface
+{
+    /**
+     * @param string $id
+     *
+     * @return $this
+     */
+    public function setId($id);
+
+    /**
+     * @return StatementInterface
+     */
+    public function getStatement();
+
+    /**
+     * @param StatementInterface $statement
+     *
+     * @return $this
+     */
+    public function setStatement($statement);
+
+    /**
+     * @return DraftStatementInterface
+     */
+    public function getDraftStatement();
+
+    /**
+     * @param DraftStatementInterface $draftStatement
+     *
+     * @return $this
+     */
+    public function setDraftStatement($draftStatement);
+
+    /**
+     * @return string
+     */
+    public function getType();
+
+    /**
+     * @param string $type
+     *
+     * @return $this
+     */
+    public function setType($type);
+
+    /**
+     * @return string
+     */
+    public function getValue();
+
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function setValue($value);
+
+}
