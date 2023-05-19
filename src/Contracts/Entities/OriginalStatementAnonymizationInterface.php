55886d17ff0f628c59c49c18bfa51cec81e66836
Saeed Jalalifar <jalalifar@demos-deutschland.de>
2023-05-19 09:10:40 +0200

    feature (refs T32212): add necessary OriginalStatementAnonymization Interface

---
+++ src/Contracts/Entities/OriginalStatementAnonymizationInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DemosEurope\DemosplanAddon\Contracts\Entities;
+
+use DateTime;
+
+interface OriginalStatementAnonymizationInterface
+{
+    public function getCreated(): DateTime;
+
+    public function isAttachmentsDeleted(): bool;
+
+    public function setAttachmentsDeleted(bool $attachmentsDeleted): void;
+
+    public function isTextVersionHistoryDeleted(): bool;
+
+    public function setTextVersionHistoryDeleted(bool $textVersionHistoryDeleted): void;
+
+    public function isTextPassagesAnonymized(): bool;
+
+    public function setTextPassagesAnonymized(bool $textPassagesAnonymized): void;
+
+    public function getStatement(): StatementInterface;
+
+    public function setStatement(StatementInterface $statement): void;
+
+    public function getCreatedBy(): UserInterface;
+
+    public function setCreatedBy(UserInterface $createdBy): void;
+
+    public function isSubmitterAndAuthorMetaDataAnonymized(): bool;
+
+    public function setSubmitterAndAuthorMetaDataAnonymized(bool $submitterAndAuthorMetaDataAnonymized): void;
+
+}
