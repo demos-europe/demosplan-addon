e7dfa959ee8fab30b3170c7eff91199941994304
Saeed Jalalifar <jalalifar@demos-deutschland.de>
2023-05-19 09:17:22 +0200

    feature (refs T32212): add necessary PriorityArea Interface

---
+++ src/Contracts/Entities/PriorityAreaInterface.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace DemosEurope\DemosplanAddon\Contracts\Entities;
+
+use League\Fractal\Resource\ArrayCollection;
+
+interface PriorityAreaInterface extends UuidEntityInterface
+{
+    /**
+     * @return string
+     */
+    public function getKey();
+
+    /**
+     * @return string
+     */
+    public function getName();
+
+    /**
+     * @param string $key
+     */
+    public function setKey($key);
+
+    /**
+     * @return string
+     */
+    public function getType();
+
+    /**
+     * @param string $type
+     */
+    public function setType($type);
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getStatements();
+
+    /**
+     * @param ArrayCollection $statements
+     */
+    public function setStatements($statements);
+
+    /**
+     * Add Statement.
+     *
+     * @param StatementInterface $statement
+     *
+     * @return bool - true if the given statement was added to this priorityArea, otherwise false
+     */
+    public function addStatement($statement);
+
+    /**
+     * Remove Statement.
+     *
+     * @param StatementInterface $statement
+     */
+    public function removeStatement($statement);
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getStatementFragments();
+
+    /**
+     * Add StatementFragment.
+     *
+     * @param StatementFragmentInterface $fragment
+     */
+    public function addStatementFragment($fragment);
+
+    /**
+     * Remove StatementFragment.
+     *
+     * @param StatementFragmentInterface $fragment
+     */
+    public function removeStatementFragment($fragment);
+
+}
