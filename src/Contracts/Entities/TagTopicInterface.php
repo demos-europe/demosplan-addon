b88410587e133b9d4f2c1ec5ad2575095d4f633d
Saeed Jalalifar <jalalifar@demos-deutschland.de>
2023-05-19 11:00:40 +0200

    feature (refs T32212): add necessary Tag Interface

---
+++ src/Contracts/Entities/TagTopicInterface.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace DemosEurope\DemosplanAddon\Contracts\Entities;
+
+use League\Fractal\Resource\Collection;
+use DateTime;
+
+interface TagTopicInterface extends UuidEntityInterface
+{
+    public const TAG_TOPIC_MISC = 'Sonstiges';
+
+    /**
+     * @return Collection<int, TagInterface>
+     */
+    public function getTags(): Collection;
+
+    /**
+     * Returns a specific tag of this topic, if exists.
+     *
+     * @param string $id identifies the tag
+     *
+     * @return TagInterface|null
+     */
+    public function getTag($id);
+
+    /**
+     * Add a specific Tag to this Topic.
+     *
+     * @param TagInterface $tag
+     *
+     * @return TagTopicInterface
+     */
+    public function addTag($tag);
+
+    /**
+     * Removes a specific Tag from this Topic.
+     *
+     * @param TagInterface $tag
+     *
+     * @return TagTopicInterface
+     */
+    public function removeTag($tag);
+
+    /**
+     * @param string|null $id
+     */
+    public function setId($id): self;
+
+    /**
+     * @return DateTime
+     */
+    public function getCreatedDate();
+
+    /**
+     * @return string
+     */
+    public function getTitle();
+
+    /**
+     * @return string
+     *
+     * @deprecated use {@link getTitle} instead
+     */
+    public function getName();
+
+    /**
+     * Set title.
+     *
+     * @param string $title
+     *
+     * @return string
+     * @return TagTopicInterface
+     */
+    public function setTitle($title);
+
+    /**
+     * @return ProcedureInterface
+     */
+    public function getProcedure();
+
+    /**
+     * @param ProcedureInterface $procedure
+     */
+    public function setProcedure($procedure);
+}
