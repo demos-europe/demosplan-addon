52a072ec6a1daab30a58854ce2438e06b04720f2
Saeed Jalalifar <jalalifar@demos-deutschland.de>
2023-05-19 11:07:26 +0200

    feature (refs T32212): add necessary UserRoleInCustomer Interface

---
+++ src/Contracts/Entities/UserRoleInCustomerInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace DemosEurope\DemosplanAddon\Contracts\Entities;
+
+interface UserRoleInCustomerInterface extends UuidEntityInterface
+{
+    /**
+     * Set User.
+     */
+    public function setUser(UserInterface $user): self;
+
+    /**
+     * Get User.
+     */
+    public function getUser(): UserInterface;
+
+    /**
+     * Set Role.
+     */
+    public function setRole(RoleInterface $role): self;
+
+    /**
+     * Get Role.
+     */
+    public function getRole(): RoleInterface;
+
+    public function setCustomer(?CustomerInterface $customer): self;
+
+    /**
+     * Get Customer.
+     *
+     * @return CustomerInterface|null
+     */
+    public function getCustomer();
+
+}
