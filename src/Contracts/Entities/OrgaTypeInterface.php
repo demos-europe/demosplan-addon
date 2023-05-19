4abb975fe73ebf36b4044e04d0c5c3fa6406b46d
Saeed Jalalifar <jalalifar@demos-deutschland.de>
2023-05-19 09:08:56 +0200

    feature (refs T32212): update OrgaType Interface

--- src/Contracts/Entities/OrgaTypeInterface.php
+++ src/Contracts/Entities/OrgaTypeInterface.php
@@ -2,7 +2,80 @@
 
 namespace DemosEurope\DemosplanAddon\Contracts\Entities;
 
+use League\Fractal\Resource\Collection;
+
 interface OrgaTypeInterface extends UuidEntityInterface
 {
+    /**
+     * AHB = Anhörungsbehörde = hearing authority.
+     *
+     * @const string Denotes a hearing authority agency
+     */
+    public const HEARING_AUTHORITY_AGENCY = 'OHAUTH';
 
+    /**
+     * @const string Denotes a public agency (Institution)
+     */
+    public const PUBLIC_AGENCY = 'OPSORG';
+
+    /**
+     * @const string Denotes a planning agency (Planungsbüro)
+     */
+    public const PLANNING_AGENCY = 'OPAUTH';
+
+    /**
+     * @const string Denotes a municipality (Fachplaner)
+     */
+    public const MUNICIPALITY = 'OLAUTH';
+
+    /**
+     * @const string Default orga type when no other fits
+     */
+    public const DEFAULT = 'OTDEFA';
+
+    public const ORGATYPE_ROLE = [
+        self::PUBLIC_AGENCY            => [
+            RoleInterface::PUBLIC_AGENCY_COORDINATION,
+            RoleInterface::PUBLIC_AGENCY_WORKER,
+        ],
+        self::MUNICIPALITY             => [
+            RoleInterface::PLANNING_AGENCY_ADMIN,
+            RoleInterface::PLANNING_AGENCY_WORKER,
+        ],
+        self::PLANNING_AGENCY          => [
+            RoleInterface::PRIVATE_PLANNING_AGENCY,
+        ],
+        self::HEARING_AUTHORITY_AGENCY => [
+            RoleInterface::HEARING_AUTHORITY_ADMIN,
+            RoleInterface::HEARING_AUTHORITY_WORKER,
+        ],
+    ];
+
+    /**
+     * Set otName.
+     *
+     * @param string $name
+     */
+    public function setName($name);
+
+    public function getName(): string;
+
+    /**
+     * Set otLabel.
+     *
+     * @param string $label
+     */
+    public function setLabel($label);
+
+    public function getLabel(): string;
+
+    /**
+     * @return Collection<int, OrgaStatusInCustomerInterface>
+     */
+    public function getOrgaStatusInCustomers();
+
+    /**
+     * @param Collection<int, OrgaStatusInCustomerInterface> $orgaStatusInCustomers
+     */
+    public function setOrgaStatusInCustomers(Collection $orgaStatusInCustomers);
 }
