a60bf86cdf53e5c1920c095f77c011e89fc09ffb
Saeed Jalalifar <jalalifar@demos-deutschland.de>
2023-05-19 08:58:10 +0200

    feature (refs T32212): add necessary GdprConsent Interface

---
+++ src/Contracts/Entities/GdprConsentInterface.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace DemosEurope\DemosplanAddon\Contracts\Entities;
+
+use DateTime;
+
+interface GdprConsentInterface extends UuidEntityInterface
+{
+    public function setId(string $id);
+
+    /**
+     * @return DateTime|null
+     */
+    public function getConsentReceivedDate();
+
+    /**
+     * @param DateTime|null $consentReceivedDate
+     */
+    public function setConsentReceivedDate($consentReceivedDate);
+
+    /**
+     * @return DateTime|null
+     */
+    public function getConsentRevokedDate();
+
+    /**
+     * @param DateTime|null $date
+     */
+    public function setConsentRevokedDate($date);
+
+    /**
+     * @return UserInterface|null
+     */
+    public function getConsentee();
+
+    /**
+     * @param UserInterface|null $consentee
+     */
+    public function setConsentee($consentee);
+
+    /**
+     * @return StatementInterface
+     */
+    public function getStatement();
+
+    /**
+     * @throws InvalidDataException
+     */
+    public function setStatement(StatementInterface $statement);
+
+    public function isConsented(): bool;
+
+    public function isConsentReceived(): bool;
+
+    public function setConsentReceived(bool $consentReceived);
+
+    public function isConsentRevoked(): bool;
+
+    public function setConsentRevoked(bool $consentRevoked);
+
+    /**
+     * @return bool true if the consent was given regarding the author data of the statement
+     */
+    public function isConsentedToAuthorData(): bool;
+
+    /**
+     * @return bool true if the consent was given regarding the submitter data of the statement
+     */
+    public function isConsentedToSubmitterData(): bool;
+
+}
