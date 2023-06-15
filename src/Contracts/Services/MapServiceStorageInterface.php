2a756de9774c3996117bcfb16d6e9f4bec65e504
mussbach <57546580+mussbach@users.noreply.github.com>
2023-06-15 14:00:18 +0200

    chore: move method to correct interface

--- src/Contracts/Services/MapServiceStorageInterface.php
+++ src/Contracts/Services/MapServiceStorageInterface.php
@@ -4,17 +4,7 @@ declare(strict_types=1);
 
 namespace DemosEurope\DemosplanAddon\Contracts\Services;
 
-use Exception;
-
 interface MapServiceStorageInterface
 {
-    /**
-     * @param array  $data
-     * @param string $procedureID
-     *
-     * @return array
-     *
-     * @throws Exception
-     */
-    public function administrationGlobalGisHandler($data, $procedureID);
+
 }
--- src/Contracts/Services/ProcedureServiceStorageInterface.php
+++ src/Contracts/Services/ProcedureServiceStorageInterface.php
@@ -5,8 +5,18 @@ declare(strict_types=1);
 namespace DemosEurope\DemosplanAddon\Contracts\Services;
 
 use DemosEurope\DemosplanAddon\Contracts\Entities\ProcedureInterface;
+use Exception;
 
 interface ProcedureServiceStorageInterface
 {
     public function administrationNewHandler(array $data, string $currentUserId): ProcedureInterface;
+    /**
+     * @param array  $data
+     * @param string $procedureID
+     *
+     * @return array
+     *
+     * @throws Exception
+     */
+    public function administrationGlobalGisHandler($data, $procedureID);
 }
