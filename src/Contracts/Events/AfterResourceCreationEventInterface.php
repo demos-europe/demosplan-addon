c29892dffbb91053a3c4874cf1d07c9400f1d74d
Christian Dressler <dressler@demos-deutschland.de>
2023-08-07 07:31:17 +0200

    refactor: add getter to event

--- src/Contracts/Events/AfterResourceCreationEventInterface.php
+++ src/Contracts/Events/AfterResourceCreationEventInterface.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace DemosEurope\DemosplanAddon\Contracts\Events;
 
+use DemosEurope\DemosplanAddon\Contracts\Entities\UuidEntityInterface;
+use EDT\Wrapping\Contracts\Types\NamedTypeInterface;
+
 interface AfterResourceCreationEventInterface
 {
+    public function getTargetEntity(): UuidEntityInterface;
 
+    public function getResourceType(): NamedTypeInterface;
 }
