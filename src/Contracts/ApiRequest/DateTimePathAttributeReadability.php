bca53325bf91f7ae61e71ee9b62f2869f44ea124
Christian Dressler <dressler@demos-deutschland.de>
2023-08-09 15:38:18 +0200

    fix: automatically use ISO 8601 for dates

--- src/Contracts/ApiRequest/DateTimePathAttributeReadability.php
+++ src/Contracts/ApiRequest/DateTimePathAttributeReadability.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace DemosEurope\DemosplanAddon\Contracts\ApiRequest;
 
+use Carbon\Carbon;
 use EDT\JsonApi\Properties\Attributes\PathAttributeReadability;
 
 class DateTimePathAttributeReadability extends PathAttributeReadability
@@ -11,7 +12,7 @@ class DateTimePathAttributeReadability extends PathAttributeReadability
     protected function assertValidValue(mixed $attributeValue): int|float|string|bool|array|null|\DateTime
     {
         if ($attributeValue instanceof \DateTime) {
-            return $attributeValue;
+            return Carbon::instance($attributeValue)->toIso8601String();
         }
 
         return parent::assertValidValue($attributeValue);
