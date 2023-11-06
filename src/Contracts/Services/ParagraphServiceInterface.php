6bcc5d0eaa6ac700b95b524ee86c62a8de03adde
breierDemos <breier@demos-deutschland.de>
2023-11-06 16:02:30 +0100

    feat (T35051): add function for type hinting in addon.

--- src/Contracts/Services/ParagraphServiceInterface.php
+++ src/Contracts/Services/ParagraphServiceInterface.php
@@ -4,7 +4,22 @@ declare(strict_types=1);
 
 namespace DemosEurope\DemosplanAddon\Contracts\Services;
 
+
+use DemosEurope\DemosplanAddon\Contracts\Entities\ParagraphInterface;
+use ReflectionException;
+
 interface ParagraphServiceInterface
 {
-
+    /**
+     * Ruft alle Kapitel eines Plandokumentes eines Verfahrens ab
+     * Die Dokumente müssen sichtbar sein (visible = true).
+     *
+     * @param string $procedureId
+     * @param string $elementId
+     *
+     * @return ParagraphInterface[]
+     *
+     * @throws ReflectionException
+     */
+    public function getParaDocumentObjectList($procedureId, $elementId): array;
 }
