d65a858efc2002459285c3a7d7dada525787c597
Alexander Marten <marten@demos-deutschland.de>
2023-11-15 16:29:35 +0100

    feat (refs T33577): additional element titles are needed in dplan-core
    improve readability by adjusting constant names

--- src/Contracts/Entities/ElementsInterface.php
+++ src/Contracts/Entities/ElementsInterface.php
@@ -15,23 +15,33 @@ interface ElementsInterface extends UuidEntityInterface, CoreEntityInterface
     public const ELEMENTS_CATEGORY_FILE = 'file'; // like "Ergänzende Unterlagen" or "Landschaftsplan-Änderung"
     public const ELEMENTS_CATEGORY_PARAGRAPH = 'paragraph'; // like "Begründung" or "Textliche Festsetzungen"
     public const ELEMENTS_CATEGORY_CATEGORY = 'category'; // created element by customer
-    public const FILE_TYPE_ARBEITSKREISPAPIER = 'Arbeitskreispapier I und II';
-    public const FILE_TYPE_ARBEITSKREISPAPIER_I = 'AKI-Papier';
-    public const FILE_TYPE_ARBEITSKREISPAPIER_II = 'AKII-Papier';
-    public const FILE_TYPE_BEGRUENDUNG = 'Begründung';
-    public const FILE_TYPE_ERGAENZENDE_UNTERLAGE = 'ergänzende Unterlage';
-    public const FILE_TYPE_FNP_AENDERUNG = 'FNP-Änderung';
-    public const FILE_TYPE_GROBABSTIMMUNGSPAPIER = 'Grobabstimmungspapier';
-    public const FILE_TYPE_GUTACHTEN = 'Gutachten';
-    public const FILE_TYPE_LAPRO_AENDERUNG = 'LaPro-Änderung';
-    public const FILE_TYPE_NIEDERSCHRIFT_GROBABSTIMMUNG_ARBEITSKREISE = 'Niederschrift (Grobabstimmung, Arbeitskreise)';
-    public const FILE_TYPE_NIEDERSCHRIFT_SONSTIGE = 'Niederschrift (sonstige)';
-    public const FILE_TYPE_PLANZEICHNUNG = 'Planzeichnung';
-    public const FILE_TYPE_SCOPING_PAPIER = 'Scopingpapier';
-    public const FILE_TYPE_SCOPING_PROTOKOLL = 'Scoping-Protokoll';
-    public const FILE_TYPE_VERORDNUNG = 'Verordnung';
-    public const FILE_TYPE_VERTEILER = 'Verteiler';
-    public const STATEMENT_TYPE_FEHLANZEIGE = 'Fehlanzeige';
+
+    public const ELEMENTS_TITLE_ARBEITSKREISPAPIER = 'Arbeitskreispapier I und II';
+    public const ELEMENTS_TITLE_ARBEITSKREISPAPIER_I = 'AKI-Papier';
+    public const ELEMENTS_TITLE_ARBEITSKREISPAPIER_II = 'AKII-Papier';
+    public const ELEMENTS_TITLE_BEGRUENDUNG = 'Begründung';
+    public const ELEMENTS_TITLE_ERGAENZENDE_UNTERLAGE = 'ergänzende Unterlage';
+    public const ELEMENTS_TITLE_FNP_AENDERUNG = 'FNP-Änderung';
+    public const ELEMENTS_TITLE_GROBABSTIMMUNGSPAPIER = 'Grobabstimmungspapier';
+    public const ELEMENTS_TITLE_GUTACHTEN = 'Gutachten';
+    public const ELEMENTS_TITLE_LAPRO_AENDERUNG = 'LaPro-Änderung';
+    public const ELEMENTS_TITLE_NIEDERSCHRIFT_GROBABSTIMMUNG_ARBEITSKREISE = 'Niederschrift (Grobabstimmung, Arbeitskreise)';
+    public const ELEMENTS_TITLE_NIEDERSCHRIFT_SONSTIGE = 'Niederschrift (sonstige)';
+    public const ELEMENTS_TITLE_PLANZEICHNUNG = 'Planzeichnung';
+    public const ELEMENTS_TITLE_SCOPING_PAPIER = 'Scopingpapier';
+    public const ELEMENTS_TITLE_SCOPING_PROTOKOLL = 'Scoping-Protokoll';
+    public const ELEMENTS_TITLE_VERORDNUNG = 'Verordnung';
+    public const ELEMENTS_TITLE_VERTEILER = 'Verteiler';
+    public const ELEMENTS_TITLE_FEHLANZEIGE = 'Fehlanzeige';
+    public const ELEMENTS_TITLE_WEITERE_INFORMATION = 'weitere Information';
+    public const ELEMENTS_TITLE_LANDSCHAFTSPLAN_AENDERUNG = 'Landschaftsplan-Änderung';
+    public const ELEMENTS_TITLE_FNP_BERICHTIGUNG = 'FNP-Berichtigung';
+    public const ELEMENTS_TITLE_TEXTLICHE_FESTSETZUNGEN = 'Textliche Festsetzungen';
+    public const ELEMENTS_TITLE_GESAMTSTELLUNGNAHME = 'Gesamtstellungnahme';
+
+
+
+
 
     /**
      * The maximum number of parents (technically) allowed when nesting {@link ElementsInterface} entities.
