81442666b23499e7fc96a9c51ef1f313b920904c
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2024-01-25 16:48:34 +0100

    feat (refs T29878): Link Interfaces in DemosplanAddon with Entities in demosPlan. These are used by demospipes

--- src/DependencyInjection/DemosPlanAddonExtension.php
+++ src/DependencyInjection/DemosPlanAddonExtension.php
@@ -8,8 +8,9 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
-class DemosPlanAddonExtension extends Extension
+class DemosPlanAddonExtension extends Extension implements PrependExtensionInterface
 {
 
     public function load(array $configs, ContainerBuilder $container)
@@ -19,5 +20,19 @@ class DemosPlanAddonExtension extends Extension
             new FileLocator(__DIR__.'/../../config')
         );
         $loader->load('services.yml');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $container->loadFromExtension('doctrine', array(
+            'orm' => array(
+                'resolve_target_entities' => array(
+                    'DemosEurope\DemosplanAddon\Contracts\Entities\ProcedureInterface' => 'demosplan\DemosPlanCoreBundle\Entity\Procedure\Procedure',
+                    'DemosEurope\DemosplanAddon\Contracts\Entities\StatementInterface' => 'demosplan\DemosPlanCoreBundle\Entity\Procedure\Procedure',
+                    'DemosEurope\DemosplanAddon\Contracts\Entities\FileContainerInterface' => 'demosplan\DemosPlanCoreBundle\Entity\File',
+                    'DemosEurope\DemosplanAddon\Contracts\Entities\UserInterface' => 'demosplan\DemosPlanCoreBundle\Entity\User\User',
+                )
+            ),
+        ));
     }
 }
