d194569417260ec40560825ed7245b620b82a890
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2024-02-02 15:17:35 +0100

    feat (refs T29878): Remove CoreEntities to Interfaces connection. It is done in Core

--- src/DependencyInjection/DemosPlanAddonExtension.php
+++ src/DependencyInjection/DemosPlanAddonExtension.php
@@ -4,18 +4,13 @@ declare(strict_types=1);
 
 namespace DemosEurope\DemosplanAddon\DependencyInjection;
 
-use DemosEurope\DemosplanAddon\Contracts\Entities\EmailAddressInterface;
-use DemosEurope\DemosplanAddon\Contracts\Entities\FileInterface;
-use DemosEurope\DemosplanAddon\Contracts\Entities\ProcedureInterface;
-use DemosEurope\DemosplanAddon\Contracts\Entities\StatementInterface;
-use DemosEurope\DemosplanAddon\Contracts\Entities\UserInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
-class DemosPlanAddonExtension extends Extension implements PrependExtensionInterface
+class DemosPlanAddonExtension extends Extension
 {
 
     public function load(array $configs, ContainerBuilder $container)
@@ -25,20 +20,5 @@ class DemosPlanAddonExtension extends Extension implements PrependExtensionInter
             new FileLocator(__DIR__.'/../../config')
         );
         $loader->load('services.yml');
-    }
-
-    public function prepend(ContainerBuilder $container)
-    {
-        $container->loadFromExtension('doctrine', array(
-            'orm' => array(
-                'resolve_target_entities' => array(
-                    ProcedureInterface::class => 'demosplan\DemosPlanCoreBundle\Entity\Procedure\Procedure',
-                    StatementInterface::class => 'demosplan\DemosPlanCoreBundle\Entity\Statement\Statement',
-                    UserInterface::class => 'demosplan\DemosPlanCoreBundle\Entity\User\User',
-                    EmailAddressInterface::class => 'demosplan\DemosPlanCoreBundle\Entity\EmailAddress',
-                    FileInterface::class => 'demosplan\DemosPlanCoreBundle\Entity\File',
-                )
-            ),
-        ));
     }
 }
