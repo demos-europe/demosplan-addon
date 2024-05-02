79e93e4053465f20d712c61b284104de72d8c167
Saeed Jalalifar <jalalifar@demos-deutschland.de>
2024-05-02 07:24:04 +0200

    feature (refs dplan-11493): add method

--- src/Contracts/Events/DeleteEmailImportedStatementEventInterface.php
+++ src/Contracts/Events/DeleteEmailImportedStatementEventInterface.php
@@ -2,7 +2,9 @@
 
 namespace DemosEurope\DemosplanAddon\Contracts\Events;
 
+use DemosEurope\DemosplanAddon\Contracts\Entities\StatementInterface;
+
 interface DeleteEmailImportedStatementEventInterface
 {
-
+    public function getStatement(): StatementInterface;
 }
