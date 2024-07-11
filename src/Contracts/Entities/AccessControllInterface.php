08a01a833adbe992d0d731db6ae4b1ee1034c17e
Saeed Jalalifar <jalalifar@demos-deutschland.de>
2024-07-11 11:44:15 +0200

    feature (refs dplan-163): - create AccessControllInterface.php

---
+++ src/Contracts/Entities/AccessControllInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace DemosEurope\DemosplanAddon\Contracts\Entities;
+
+interface AccessControllInterface
+{
+    public function getPermissionName(): string;
+    public function setPermissionName(string $permission): void;
+    public function setOrga(?OrgaInterface $orga);
+    public function setRole(?RoleInterface $role);
+    public function setCustomer(?CustomerInterface $customer);
+}
