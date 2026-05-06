0cc0b644a4f75c93774cb700576b4f1358743491
breierDemos <breier@demos-deutschland.de>
2026-05-06 11:36:16 +0200

    feat(refs: DPLAN-16766): extend ProcedurePhaseDefinitionServiceInterface with phase lookup methods

--- src/Contracts/Services/ProcedurePhaseDefinitionServiceInterface.php
+++ src/Contracts/Services/ProcedurePhaseDefinitionServiceInterface.php
@@ -12,9 +12,20 @@ declare(strict_types=1);
 
 namespace DemosEurope\DemosplanAddon\Contracts\Services;
 
+use DemosEurope\DemosplanAddon\Contracts\Entities\CustomerInterface;
 use DemosEurope\DemosplanAddon\Contracts\Entities\ProcedurePhaseDefinitionInterface;
 
 interface ProcedurePhaseDefinitionServiceInterface
 {
     public function findById(string $id): ?ProcedurePhaseDefinitionInterface;
+
+    /** @return ProcedurePhaseDefinitionInterface[] */
+    public function getInternalPhaseDefinitionsForCurrentCustomer(): array;
+
+    /** @return ProcedurePhaseDefinitionInterface[] */
+    public function getExternalPhaseDefinitionsForCurrentCustomer(): array;
+
+    public function findEvaluatingDefinition(string $audience, ?CustomerInterface $customer): ?ProcedurePhaseDefinitionInterface;
+
+    public function findByNameAndAudienceAndCustomer(string $name, string $audience, CustomerInterface $customer): ?ProcedurePhaseDefinitionInterface;
 }
